73c81a5b4adb76570de3baf0f4cc7a7d6a3ec00d
Lauriel Mesquita <laurielmesquita@me.com>
2020-11-30 13:17:09 -0300

    Gatsby Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -7,6 +8,19 @@ const IndexPage = () => (
   <Layout>
     <SEO title='Home' />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <Link to='/' activeStyle={{ color: 'red' }}> Home
+        </Link>
+      </li>
+      <li>
+        <a href='/about'>About</a>
+      </li>
+      <li>
+        <Link to='/about' activeStyle={{ color: 'red' }}> About (Gatsby Link)
+        </Link>
+      </li>
+    </ul>
   </Layout>
 )
 
